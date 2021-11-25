867600ab66f55edeedafcb90cdaf465cd4f08a36
Fabio <fabioaoad@gmail.com>
2021-11-25 16:59:35 -0300

    Leer información de un archivo JSON - Tareas por hacer - S05A44

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,44 @@
+
+
+const fs = require('fs');
+
+
+let listadoPorHacer = [];
+
+const guardarBD = () =>{
+
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile(`db/data.json`, data,(err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+    });
+}
+
+const crear = (descripcion) => {
+    
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarBD();
+    return porHacer;
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+        //console.log(listadoPorHacer);
+    }
+    catch (error) {
+        listadoPorHacer = [];
+    }
+
+}
+
+
+module.exports = {
+    crear,
+
+}
