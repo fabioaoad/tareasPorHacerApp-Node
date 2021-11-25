31e0f2709a70a531beb75e22d9060cbca3f99cf9
Fabio <fabioaoad@gmail.com>
2021-11-25 17:52:45 -0300

    Optimizacion de codigo en yargs - Tareas por hacer - S05A48

--- config/yargs.js
+++ config/yargs.js
@@ -1,32 +1,35 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+
+const completado = {
+    default: true,
+    alias: 'c',
+    des: 'Marca como completado o pendiente la tarea'
+};
+
+
+
 const argv = require('yargs')
     .command('crear','Crear un elemento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        }
+        descripcion
     })
     .command('actualizar','Actualiza el estado completado de una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        },
-        completado: {
-            default: true,
-            alias: 'c',
-            des: 'Marca como completado o pendiente la tarea'
-        }
+        descripcion,
+        completado
     })
     .command('borrar','Borra una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        }
+        descripcion
     })
     .help()
     .argv;
+
+
+
+
 
 
 module.exports = {
